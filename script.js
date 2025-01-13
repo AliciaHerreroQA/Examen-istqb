39be84252166b6dd20c483a8d622bc9c2e8d51ee
Alicia <alihersan@alu.edu.gva.es>
2025-01-13 23:00:56 +0100

    Historia de usuario 1

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//Aqui va tu codigo javascript
+//Aquí va tu codigo javascript
+function inscripcion() {
+    let nombre =document.getElementById("nombre").value.trim();
+    let apellidos =document.getElementById("apellidos").value.trim();
+    let mensaje = "Por favor, completa todos los campos de forma correcta"
+
+//Para que no se quede el formulario vacío
+if (nombre === "" || apellidos === "" ) {
+    document.getElementById("formsingup").textContent =mensaje
+}else {
+    //Para mostrar un mensaje de bienvenida en mayúsculas
+    document.getElementById("formsingup").textContent = "¡BIENVENIDO " + nombre.toUpperCase() + " " +
+    apellidos.toUpperCase() + "!";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+   
+
+
+
+
