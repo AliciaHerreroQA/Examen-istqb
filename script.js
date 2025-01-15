682f10ef32e96fbd99803adb52fbcac6e1a07cd1
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2025-01-15 10:43:46 +0000

    correcciones entrega 1

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ function inscripcion() {
     let mensaje = "Por favor, completa todos los campos de forma correcta"
 
 //Para que no se quede el formulario vacío
-if (nombre === "" || apellidos === "" ) {
-    document.getElementById("formsingup").textContent =mensaje
+//Cambia la validación. Puedes usar document.getElementById("formsingup").checkValidity() y te comprueba de forma automatica el formulario sin tener que ver si nombre o apellidos es vacio.
+//Aqui si no es valido, muestra el mensaje. No uses formsingup para mostrar el mensaje porque te va a desaparecer el formulario. Mejor crea un elemento nuevo para poner el mensaje.
+if (!document.getElementById("formsingup").checkValidity() ) {
+    document.getElementById("formMessage").textContent =mensaje
 }else {
-    //Para mostrar un mensaje de bienvenida en mayúsculas
+    //Aqui tambien usa el elemento formMessage para mostrar el mensaje en este caso de bienvenida
     document.getElementById("formsingup").textContent = "¡BIENVENIDO " + nombre.toUpperCase() + " " +
     apellidos.toUpperCase() + "!";
+   //Aqui ocultas el formulario
+   document.getElementById("formsingup").style.display = "none";
+   //Aqui muestras la seccion de examen
+   document.getElementById("exam").style.display = "block";
+
 }
-
-
-
-
+}
+   
 
 
 
