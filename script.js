4f4fd22748185f3232bec3bb8f135e9f3e2b3c79
Alicia <alihersan@alu.edu.gva.es>
2025-02-05 02:30:10 +0100

    correccion historia 1

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 //Aquí va tu codigo javascript
-function inscripcion() {
-    let nombre =document.getElementById("nombre").value.trim();
-    let apellidos =document.getElementById("apellidos").value.trim();
-    let mensaje = "Por favor, completa todos los campos de forma correcta"
 
-//Para que no se quede el formulario vacío
-if (nombre === "" || apellidos === "" ) {
-    document.getElementById("formsingup").textContent =mensaje
-}else {
-    //Para mostrar un mensaje de bienvenida en mayúsculas
-    document.getElementById("formsingup").textContent = "¡BIENVENIDO " + nombre.toUpperCase() + " " +
-    apellidos.toUpperCase() + "!";
+function bienvenida() {
+
+    //para validar, se hace al final, pero lo colocamos al inicio
+    let formulario = document.getElementById("formsingup")
+
+    if(formulario.checkValidity()) {                                          //.checkValidity valida el formulario. Si el formulario es válido, hace todo lo siguiente, sino-else
+
+    let nombre = document.getElementById("nombre").value    //.trim();   ponemos el id que hayamos puesto en el input del html
+    let apellido = document.getElementById("apellidos").value    //.trim();
+
+
+    let mensajeBienvenida = "Bienvenido " + nombre + " " + apellido  //mensaje de bienvenida
+
+    document.getElementById("mensaje").textContent = mensajeBienvenida.toUpperCase()  //mensaje de bienvenida en mayúscula
+
+    document.getElementById("formsingup").style.display = "none";   //para ocultar el formulario. 
+
+    document.getElementById("exam").style.display = "block";  //para mostrar, seccion exam
+
+    }                         
+    else {
+        document.getElementById("mensaje").textContent = "Error en formulario. Los campos son obligatorios y deben tener entre 3 y 30 caracteres."
+    }
+    
 }
 
 
@@ -25,7 +38,8 @@ if (nombre === "" || apellidos === "" ) {
 
 
 
-}
+
+
    
 
 
